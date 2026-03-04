45ffc1df20247a75e723320efb80691d5f061d8b
maeda-takumi <jiangqiantian54@gmail.com>
2026-03-04 15:42:36 +0900

    06

--- config.php
+++ config.php
@@ -4,4 +4,5 @@
     define('DB_USER', 'ss911157_sedo');
     define('DB_PASS', 'sedorisedori');
     define('DB_CHARSET', 'utf8mb4');
+    define('GEMINI_API_KEY','AIzaSyDRWax2idqnomxoGkDg0hoUQt0cpEEMKjs')
 ?>
